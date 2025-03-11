10188576f579615b7a5e3d7a48fd52d23e164613
Lopez Hortelano external <juan-carlos.lopez-hortelano.external@capgemini.com>
2025-03-11 09:54:08 +0100

    Feat: Author

---
+++ tutorialauthor/src/main/java/com/ccsw/tutorialauthor/TutorialauthorApplication.java
@@ -0,0 +1,13 @@
+package com.ccsw.tutorialauthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialauthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialauthorApplication.class, args);
+	}
+
+}
